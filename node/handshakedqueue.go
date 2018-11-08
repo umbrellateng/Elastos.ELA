c27d6b4f241874c425993536aecb5f5d932b124b
hejianhui <hejianhui@elastos.org>
2018-11-08 11:25:02 +0800

    no

--- node/handshakedqueue.go
+++ node/handshakedqueue.go
@@ -34,7 +34,7 @@ func (q *handshakeQueue) AddToHandshakeQueue(addr string, node protocol.Noder) {
 	q.Lock()
 	q.conns[node] = node.GetConn()
 	q.Unlock()
-	log.Infof("addr:", addr, "node:%p", node, "conn:", q.conns[node])
+	log.Infof("addr:", addr, "node: %p", node, "conn:", q.conns[node])
 	log.Info("add to handshake queue 3")
 	// Close handshake timeout connections
 	go q.handleTimeout(addr, node)
@@ -54,7 +54,7 @@ func (q *handshakeQueue) handleTimeout(addr string, node protocol.Noder) {
 	time.Sleep(time.Second * protocol.HandshakeTimeout)
 	q.Lock()
 	log.Info("handle timeout 1", addr)
-	log.Info("addr:", addr, "node:", node)
+	log.Infof("addr:", addr, "node: %p", node)
 	for _, v := range q.conns {
 		log.Info("q.conn:", v)
 	}
